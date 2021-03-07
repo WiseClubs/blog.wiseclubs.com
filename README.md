3cfdae5c01ca8317d4b38c5ad1fb154f5411b415
Nishant Mendiratta <nishuarora.nishant@gmail.com>
2021-03-07 09:36:57 +0800

    init blog

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# blog.wiseclubs.com
-Blog for wiseclubs.com
+-Blog for wiseclubs.com
+
+#### Getting Started
+Created from vercel starter template using following command. 
+`> npx create-next-app . --use-npm --example "https://github.com/vercel/next-learn-starter/tree/master/learn-starter"`
+
+#### Run the development server
+To start blog.wiseclubs.com on local. It will start server on `0.0.0.0:3000`, url: `http://localhost:3000`
+`> /Users/blog.wiseclubs.com`
+`npm run dev`
+
+Inside that directory, you can run several commands:
+1. Starts the development server. 
+`npm run dev` 
+2. Builds the app for production.
+`npm run build`
+3. Runs the built app in production mode.
+`npm start`
+
---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     }
+//   }
+// }
+
+// import useSWR from 'swr'
+
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch)
+
+//   if (error) return <div>failed to load</div>
+//   if (!data) return <div>loading...</div>
+//   return <div>hello {data.name}!</div>
+// }
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi, I'm a senior frontend developer, specialized in high-performance web applications. In the last 6+ years, I've worked in various domains be it the fastest growing health tech startup like HealthTap or giant e-commerce like Snapdeal. I've experience working with smart and diverse teams. Javascript is my forte, I'm proficient in most of the cutting-edge technologies like React/Redux & Node.
+        </p>
+        <p className='bg-gray-300 p-2 rounded mt-2 text-gray-500 text-xs'>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
